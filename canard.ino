d16e9cc89aabb760de0e29c6132dc416be0dfe64
Albert Zheng <106856325+albertzheng6@users.noreply.github.com>
2022-12-10 13:35:54 -0500

    Update canard.ino

--- canard.ino
+++ canard.ino
@@ -1,31 +1,10 @@
-/*
-* Brian R Taylor
-* brian.taylor@bolderflight.com
-* 
-* Copyright (c) 2021 Bolder Flight Systems Inc
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the “Software”), to
-* deal in the Software without restriction, including without limitation the
-* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-* sell copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-* IN THE SOFTWARE.
-*/
-
 #include "mpu9250.h" //install zip file from bolder github page: https://github.com/bolderflight/invensense-imu
 
+/* Mpu9250 object */
+bfs::Mpu9250 imu;
+
 void setup() {
+  //make sure baud rate is same is baud rate setting in serial monitor
   Serial.begin(115200); // initialize serial port and set data rate (in bits per sec)
   while(!Serial) {
     // wait for serial port to connect
@@ -33,7 +12,6 @@ void setup() {
   Wire.begin(); //initialize Wire library for I2C communication
   Wire.setClock(400000); // change clock frequency in Hz
 
-  //Not sure what the rest does
   /* I2C bus,  0x68 address */
   imu.Config(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM);
   /* Initialize and configure IMU */
@@ -49,8 +27,41 @@ void setup() {
 
 }
 
+// continuously print current state of mpu9250
 void loop() {
+
+  // imu.Read() reads data from MPU9250 and stores data in MPU9250 object. Returns true if data is successfully read, otherwise false
   if(imu.Read()) {
+
+    //print to serial monitor
+    //Serial.print(imu.new_imu_data()); // Returns true if new data was returned from the accelerometer and gyro.
+    //Serial.print("\t");
+
+    Serial.print("accel_x: ");
+    Serial.print(100.0*imu.accel_x_mps2()); // x acceleration (m/s/s) from the Mpu9250 object
+    Serial.print("\t");
+    
+    Serial.print("accel_y: ");
+    Serial.print(100.0*imu.accel_y_mps2()); // y acceleration (m/s/s) from the Mpu9250 object
+    Serial.print("\t");
+    
+    Serial.print("accel_z: ");
+    Serial.print(100.0*imu.accel_z_mps2()); // z acceleration (m/s/s) from the Mpu9250 object
+    Serial.print("\t");
+    
+    Serial.print("gyro_x: ");
+    Serial.print(100.0*imu.gyro_x_radps()); //angular velocity around x axis (rad/s) from the Mpu9250 object
+    Serial.print("\t");
+    
+    Serial.print("gyro_y: ");
+    Serial.print(100.0*imu.gyro_y_radps()); //angular velocity around y axis (rad/s) from the Mpu9250 object
+    Serial.print("\t");
+    
+    Serial.print("gyro_z: ");
+    Serial.print(100.0*imu.gyro_z_radps()); //angular velocity around z axis (rad/s) from the Mpu9250 object
+    Serial.print("\n"); // next line
+
+    //send values to odrive
     
   }
 }
