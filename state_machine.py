6585cb8a1ea92696ba3ea7fc9cc20279a0856b74
Albert Zheng <106856325+azheng5@users.noreply.github.com>
2022-12-21 20:45:51 -0500

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -1,103 +1,130 @@
-import time
+import numpy as np
+import sys
+import pandas as pd
 
 # A simulation for the state machine program for GNC
 # There are 6 states during rocket flight. During each state, the corresponding code for that state and the state only
 # runs in a loop, until a state transition has been detected.
+#simulation data time step: 0.05 s
 
-def get_accel_z():
-    # get current accel z
-    print('accel_z: ')
-    x = int(input())
-    return x
+##### Setup #####
+np.set_printoptions(threshold=sys.maxsize) # print entire array
+#np.set_printoptions(threshold = False) # print truncated array
+df = pd.read_csv('gnc_sim.csv') # convert csv file into dataframe
+data = df.loc[:,:].to_numpy() # convert dataframe into numpy array
+time_data = data[:,0]
+alt_data = data[:,1]
+accel_z_data = data[:,2]
+ind = 0 # global variable that keeps track of time
 
-def get_alt():
-    # get current altitude
-    print('get_alt: ')
-    x = int(input())
-    return x
 
-# 1. Pre launch - rocket sitting on launchpad
+def get_time(x):
+    return time_data[x]
+def get_alt(x):
+    return alt_data[x]
+def get_accel_z(x):
+    return accel_z_data[x]
+
+print("START OF FLIGHT")
+
+##### 1.Pre-launch #####
 print("Current state: Pre launch...")
 while True:
     breakLoop = False
 
     # Detect state transition: liftoff
     count = 0
-    accel_z = get_accel_z()
-    while accel_z >= 5:
+    accel_z = get_accel_z(ind)
+    while accel_z >= 10:
         count = count + 1
-        time.sleep(0.1)
-        if count >= 2:
-            print("Liftoff detected")
+
+        ind = ind + 1
+        print(time_data[ind])
+
+        if count >= 8:
+            t = get_time(ind)
+            print(f"Liftoff detected at {t} s")
             breakLoop = True
             break
 
-        accel_z = get_accel_z()
+        accel_z = get_accel_z(ind)
 
     if breakLoop == True:
         break
 
-# 2. Powered flight - rocket under
-print("Current state: Powered flight...")
-while True:
-    breakLoop = False
+    ind = ind + 1
+    print(time_data[ind])
 
-    # Run control algo
+##### 2.Powered ascent #####
+print("Current state: Powered ascent...")
+for x in range(0, 10): # delay to ensure initial negative accel z isnt captured
+    ind = ind + 1
+    print(time_data[ind])
+
+while True:
 
     # Detect state transition: engine burnout
-    count = 0
-    accel_z = get_accel_z()
-    while accel_z <= 2:
-        count = count + 1
-        time.sleep(0.1)
-        if count >= 2:
-            print("Engine burnout detected")
-            breakLoop = True
-            break
-
-        accel_z = get_accel_z()
-
-    if breakLoop == True:
+    accel_z = get_accel_z(ind)
+    if accel_z < 0: # negative accel z -> engine burnout
+        t = get_time(ind)
+        print(f"Engine burnout detected at {t} s")
         break
 
-# 3. Unpowered flight
+    ind = ind + 1
+    print(time_data[ind])
+
+##### 3.Unpowered ascent #####
 # State transition: detect apogee
-print("Current state: Unpowered flight...")
-old_alt = get_alt()
+print("Current state: Unpowered ascent...")
+old_alt = get_alt(ind)
 while True:
 
-    time.sleep(1)  # wait 1 sec
-    current_alt = get_alt()
+    # Detect state transition: apogee
 
+    for x in range(0,10):
+        ind = ind + 1
+        print(time_data[ind])
+
+    current_alt = get_alt(ind)
     if current_alt < old_alt:
-        print("Apogee detected")
+        apogee = current_alt
+        t = get_time(ind)
+        print(f"Apogee detected at {t} s")
         break
 
     old_alt = current_alt
 
-
-
-# 4. Free fall
+##### 4.Free fall #####
 # State transition: detect pyro fire
 print("Current state: Free fall")
 while True:
 
-    alt = get_alt()
+    # Detect state transition: chute deployment
 
-    if alt < 25:
-        print("Chutes deployed")
+    ind = ind + 1
+    print(time_data[ind])
+
+    alt = get_alt(ind)
+    if alt < apogee:
+        t = get_time(ind)
+        print(f"Chutes deployed at {t} s")
         break
 
 
-
-# 5. Chute descent
-# State transition: detect landing
+##### 5.Chute descent #####
 print("Current state: Chute descent")
 while True:
 
-    accel_z = get_accel_z()
-    if accel_z >= -1 and accel_z <= 1:
-        print("Landing")
+    ind = ind + 1
+    print(time_data[ind])
+
+    # Detect state transition: landing
+    alt = get_alt(ind)
+    accel_z = get_accel_z(ind)
+    if (accel_z >= -1 and accel_z <= 1) or (alt < 10):
+        t = get_time(ind)
+        print(f"Landing detetected at {t} s")
         break
 
-# 6. Landing: rocket has landed
+##### 6.Landing: rocket has landed #####
+print("END OF FLIGHT")
